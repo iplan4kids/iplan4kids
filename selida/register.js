3012712540bb53600539daaf7efd61d62327facc
barbabrill <billbrillakis@gmail.com>
2018-02-20 20:32:53 +0200

    register allages kai sthn forma dimiourgias drastiriotitas + allagi tou path twn eikonwn

--- selida/register.js
+++ selida/register.js
@@ -24,3 +24,30 @@ var check = function() {
     //document.getElementById('message').innerHTML = 'not matching';
   }
 }
+
+var check1 = function() {
+  if (document.getElementById('password1').value == "" || document.getElementById('confirm_password1').value == ""){
+    
+    document.getElementById('password1').style.border = '1px solid #ddd';
+    document.getElementById('password1').style.backgroundColor = "white";
+    document.getElementById('confirm_password1').style.border = '1px solid #ddd';
+    document.getElementById('confirm_password1').style.backgroundColor = "white";
+    document.getElementById('register-submit').disabled = false;
+  }
+  else if (document.getElementById('password1').value ==
+    document.getElementById('confirm_password1').value) {
+    document.getElementById('password1').style.border = '1px solid #ddd';
+    document.getElementById('password1').style.backgroundColor = "white";
+    document.getElementById('confirm_password1').style.border = '1px solid #ddd';
+    document.getElementById('confirm_password1').style.backgroundColor = "white";
+    document.getElementById('register-submit').disabled = false;
+    //document.getElementById('message').innerHTML = 'matching';
+  } else {
+    document.getElementById('password1').style.borderBottom = "3px solid red";
+    document.getElementById('password1').style.backgroundColor = "lightgrey";
+    document.getElementById('confirm_password1').style.borderBottom = "3px solid red";
+    document.getElementById('confirm_password1').style.backgroundColor = "lightgrey";
+    document.getElementById('register-submit').disabled = true;
+    //document.getElementById('message').innerHTML = 'not matching';
+  }
+}
