b744439e3113c9b2fbbd0885546d0003556de000
BarbaBrill <billbrillakis@gmail.com>
2018-02-28 02:44:36 +0200

    forma_drastiriotitas done

--- selida/registerevent.js
+++ selida/registerevent.js
@@ -91,8 +91,8 @@ var componentForm = {
     street_number: 'short_name',
     route: 'long_name',
     locality: 'long_name',
-    administrative_area_level_1: 'short_name',
-    country: 'long_name',
+    //administrative_area_level_1: 'short_name',
+   // country: 'long_name',
     postal_code: 'short_name'
 };
 
@@ -102,7 +102,7 @@ function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(
         /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
         {types: ['geocode']});
-
+    autocomplete.setComponentRestrictions({'country': 'gr'});
     // When the user selects an address from the dropdown, populate the address
     // fields in the form.
     autocomplete.addListener('place_changed', fillInAddress);
@@ -110,22 +110,63 @@ function initAutocomplete() {
 
 function fillInAddress() {
     // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
+    var place = autocomplete.getPlace() ;
 
-    for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-    }
+
+    document.getElementById('locality').value = "";
+    document.getElementById('route').value = "";
+    document.getElementById('street_number').value = "" ;
+    document.getElementById('postal_code').value = "" ;
+
+
+    var location = place.geometry.location;
+    var lang = location.lng();
+    var lat = location.lat();
+
+    document.getElementById('lat').value = lat;
+    document.getElementById('lng').value = lang;
+
+    /* for (var component in componentForm) {
+         document.getElementById(component).value = '';
+         document.getElementById(component).disabled = false;
+     }
+     */
+
+
+
 
     // Get each component of the address from the place details
     // and fill the corresponding field on the form.
     for (var i = 0; i < place.address_components.length; i++) {
         var addressType = place.address_components[i].types[0];
+
         if (componentForm[addressType]) {
+
             var val = place.address_components[i][componentForm[addressType]];
+
             document.getElementById(addressType).value = val;
         }
     }
+    $("#geowrong").empty();
+    var geoDiv = $("#geowrong");
+    var msgwrong = "";
+    if (!$("#locality").val()){
+        msgwrong += " Πόλη <br>";
+    }
+    if (!$("#route").val()){
+        msgwrong += " Οδό <br>";
+    }
+    if (!$("#street_number").val() ){
+        msgwrong += " Αριθμό οδού <br>";
+    }
+    if (msgwrong !="" ){
+        msgwrong = "*Πρέπει να συμπεριλάβετε τα εξής: <br>"+msgwrong;
+        geoDiv.html(msgwrong);
+
+    }
+    var codeb = $("#postal_code").val() ;
+    var codea = codeb.replace(/\s/g, "");
+    $("#postal_code").val(codea) ;
 }
 
 
@@ -134,7 +175,7 @@ function fillInAddress() {
 
 // Bias the autocomplete object to the user's geographical location,
 // as supplied by the browser's 'navigator.geolocation' object.
-function geolocate() {
+/*function geolocate() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             var geolocation = {
@@ -149,7 +190,4 @@ function geolocate() {
         });
     }
 }
-
-
-
-
+*/
