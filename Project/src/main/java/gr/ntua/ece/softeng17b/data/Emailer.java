90b28e5b575acf29bf5484816ef4f9de1302a06d
vasiliskv <v.kavardinas@gmail.com>
2018-03-07 13:31:44 +0200

    emailer

---
+++ Project/src/main/java/gr/ntua/ece/softeng17b/data/Emailer.java
@@ -0,0 +1,66 @@
+package gr.ntua.ece.softeng17b.data;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.util.Properties;
+
+public class Emailer {
+
+    private String from;
+    private Session session;
+
+    public void setup(boolean auth, boolean enable, String host, int port , String username, String password, String from) {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        this.session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+        this.from = from;
+    }
+
+    public boolean sendEmail(String to, String subject, String text, String msg){
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+            message.setSubject(subject);
+
+            BodyPart part1=new MimeBodyPart();
+            part1.setText(msg);
+
+
+            File file=new File("/home/mano/temp/Myfile.doc");
+            BodyPart part2=new MimeBodyPart();
+            DataSource attachment=new FileDataSource(file);
+            part2.setDataHandler(new DataHandler(attachment));
+            part2.setFileName(file.getName());
+
+
+            Multipart multiPart=new MimeMultipart();
+            multiPart.addBodyPart(part1);
+            multiPart.addBodyPart(part2);
+
+            message.setContent(multiPart);
+
+            Transport.send(message);
+            System.out.println("Message with attachment sent successfully....");
+            return true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
