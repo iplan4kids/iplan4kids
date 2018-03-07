604847773d6d929e3ce747d6456825c2bb9ec6dc
BarbaBrill <billbrillakis@gmail.com>
2018-03-07 19:32:19 +0200

    part2 allages sta admin.js,adminPage.html,styles.css,allEvents.js

--- Project/src/main/webapp/pages/admin.js
+++ Project/src/main/webapp/pages/admin.js
@@ -1,442 +1,30 @@
 //------------------------------------------ ADMIN STUFF ABOUT USERS-PROVIDERS ------------------------------------------------
 
-var mock_data_1 = [{
-    "id": 1,
-    "name": "Lorens",
-    "username": "lbligh0",
-    "email": "lhosburn0@msu.edu",
-    "phone": "442-431-1318"
-}, {
-    "id": 2,
-    "name": "Mira",
-    "username": "mhiley1",
-    "email": "mgirardetti1@edublogs.org",
-    "phone": "793-572-0484"
-}, {
-    "id": 3,
-    "name": "Seana",
-    "username": "sackerman2",
-    "email": "sbeamish2@acquirethisname.com",
-    "phone": "639-100-8436"
-}, {
-    "id": 4,
-    "name": "Christy",
-    "username": "cdoick3",
-    "email": "cspellsworth3@bloglovin.com",
-    "phone": "298-103-8921"
-}, {
-    "id": 5,
-    "name": "Rowland",
-    "username": "ryaldren4",
-    "email": "rskittle4@psu.edu",
-    "phone": "187-897-9851"
-}, {
-    "id": 6,
-    "name": "Zahara",
-    "username": "zside5",
-    "email": "zgoodisson5@webnode.com",
-    "phone": "250-827-5390"
-}, {
-    "id": 7,
-    "name": "Alexandra",
-    "username": "aoldacres6",
-    "email": "apoad6@uiuc.edu",
-    "phone": "958-143-9517"
-}, {
-    "id": 8,
-    "name": "Robena",
-    "username": "rtennet7",
-    "email": "rhawkin7@cloudflare.com",
-    "phone": "924-697-1846"
-}, {
-    "id": 9,
-    "name": "Raffaello",
-    "username": "rrucklidge8",
-    "email": "rdaffey8@noaa.gov",
-    "phone": "861-538-3194"
-}, {
-    "id": 10,
-    "name": "Alane",
-    "username": "abaudinet9",
-    "email": "adoddrell9@ifeng.com",
-    "phone": "735-727-9892"
-}, {
-    "id": 11,
-    "name": "Levi",
-    "username": "lmaffettia",
-    "email": "lpeschmanna@senate.gov",
-    "phone": "635-758-8720"
-}, {
-    "id": 12,
-    "name": "Haze",
-    "username": "hamdohrb",
-    "email": "hyeomanb@cmu.edu",
-    "phone": "426-931-1891"
-}, {
-    "id": 13,
-    "name": "Bernardine",
-    "username": "bmaylerc",
-    "email": "bshealc@hexun.com",
-    "phone": "389-804-8090"
-}, {
-    "id": 14,
-    "name": "Derwin",
-    "username": "dsquierd",
-    "email": "diliffed@uol.com.br",
-    "phone": "729-469-5926"
-}, {
-    "id": 15,
-    "name": "Anne-corinne",
-    "username": "acreesee",
-    "email": "adalesioe@webs.com",
-    "phone": "278-866-6311"
-}, {
-    "id": 16,
-    "name": "Kary",
-    "username": "kstockwellf",
-    "email": "keasbyf@dot.gov",
-    "phone": "330-760-0175"
-}, {
-    "id": 17,
-    "name": "Michele",
-    "username": "mokieltg",
-    "email": "mmotteg@vimeo.com",
-    "phone": "411-290-8227"
-}, {
-    "id": 18,
-    "name": "Reinwald",
-    "username": "rlombardoh",
-    "email": "rcoulth@godaddy.com",
-    "phone": "405-923-8391"
-}, {
-    "id": 19,
-    "name": "Tabbi",
-    "username": "tnestori",
-    "email": "tgowari@scribd.com",
-    "phone": "145-655-5217"
-}, {
-    "id": 20,
-    "name": "Ceil",
-    "username": "cthumannj",
-    "email": "cchessunj@howstuffworks.com",
-    "phone": "586-193-0476"
-}, {
-    "id": 21,
-    "name": "Harlene",
-    "username": "hcampagnek",
-    "email": "hjeroschk@archive.org",
-    "phone": "243-758-3577"
-}, {
-    "id": 22,
-    "name": "Ernst",
-    "username": "estrondl",
-    "email": "epassfieldl@csmonitor.com",
-    "phone": "829-305-9328"
-}, {
-    "id": 23,
-    "name": "Darsie",
-    "username": "doldfieldm",
-    "email": "djuaram@biblegateway.com",
-    "phone": "635-110-2385"
-}, {
-    "id": 24,
-    "name": "Aridatha",
-    "username": "akennyn",
-    "email": "ahaddockn@marriott.com",
-    "phone": "517-440-0230"
-}, {
-    "id": 25,
-    "name": "Analise",
-    "username": "acrystalo",
-    "email": "afazackerleyo@shareasale.com",
-    "phone": "314-299-4898"
-}, {
-    "id": 26,
-    "name": "Nicolas",
-    "username": "norpwoodp",
-    "email": "nvirginp@blogger.com",
-    "phone": "606-353-1532"
-}, {
-    "id": 27,
-    "name": "Mose",
-    "username": "mklawiq",
-    "email": "moconnollyq@nymag.com",
-    "phone": "630-993-1118"
-}, {
-    "id": 28,
-    "name": "Daisi",
-    "username": "dtroppmannr",
-    "email": "dsergeantr@vkontakte.ru",
-    "phone": "381-138-5867"
-}, {
-    "id": 29,
-    "name": "Pat",
-    "username": "pshorys",
-    "email": "psuters@tmall.com",
-    "phone": "543-121-2704"
-}, {
-    "id": 30,
-    "name": "Coral",
-    "username": "cmcmickant",
-    "email": "cjolint@blinklist.com",
-    "phone": "668-403-7732"
-
-}
-
-];
-
-
-//------------------------------------------ ADMIN STUFF ABOUT USERS-PARENTS ------------------------------------------------
-
-
-
-var mock_data_2 = [{
-    "id": 1,
-    "first_name": "Lorens",
-    "last_name": "Lorens",
-    "username": "lbligh0",
-    "email": "lhosburn0@msu.edu",
-    "phone": "442-431-1318"
-}, {
-    "id": 2,
-    "first_name": "Mira",
-    "last_name": "Lorens",
-    "username": "mhiley1",
-    "email": "mgirardetti1@edublogs.org",
-    "phone": "793-572-0484"
-}, {
-    "id": 3,
-    "first_name": "Seana",
-    "last_name": "Lorens",
-    "username": "sackerman2",
-    "email": "sbeamish2@acquirethisname.com",
-    "phone": "639-100-8436"
-}, {
-    "id": 4,
-    "first_name": "Christy",
-    "last_name": "Lorens",
-    "username": "cdoick3",
-    "email": "cspellsworth3@bloglovin.com",
-    "phone": "298-103-8921"
-}, {
-    "id": 5,
-    "first_name": "Rowland",
-    "last_name": "Lorens",
-    "username": "ryaldren4",
-    "email": "rskittle4@psu.edu",
-    "phone": "187-897-9851"
-}, {
-    "id": 6,
-    "first_name": "Zahara",
-    "last_name": "Lorens",
-    "username": "zside5",
-    "email": "zgoodisson5@webnode.com",
-    "phone": "250-827-5390"
-}, {
-    "id": 7,
-    "first_name": "Alexandra",
-    "last_name": "Lorens",
-    "username": "aoldacres6",
-    "email": "apoad6@uiuc.edu",
-    "phone": "958-143-9517"
-}, {
-    "id": 8,
-    "first_name": "Robena",
-    "last_name": "Lorens",
-    "username": "rtennet7",
-    "email": "rhawkin7@cloudflare.com",
-    "phone": "924-697-1846"
-}, {
-    "id": 9,
-    "first_name": "Raffaello",
-    "last_name": "Lorens",
-    "username": "rrucklidge8",
-    "email": "rdaffey8@noaa.gov",
-    "phone": "861-538-3194"
-}, {
-    "id": 10,
-    "first_name": "Alane",
-    "last_name": "Lorens",
-    "username": "abaudinet9",
-    "email": "adoddrell9@ifeng.com",
-    "phone": "735-727-9892"
-}, {
-    "id": 11,
-    "first_name": "Levi",
-    "last_name": "Lorens",
-    "username": "lmaffettia",
-    "email": "lpeschmanna@senate.gov",
-    "phone": "635-758-8720"
-}, {
-    "id": 12,
-    "first_name": "Haze",
-    "last_name": "Lorens",
-    "username": "hamdohrb",
-    "email": "hyeomanb@cmu.edu",
-    "phone": "426-931-1891"
-}, {
-    "id": 13,
-    "first_name": "Bernardine",
-    "last_name": "Lorens",
-    "username": "bmaylerc",
-    "email": "bshealc@hexun.com",
-    "phone": "389-804-8090"
-}, {
-    "id": 14,
-    "first_name": "Derwin",
-    "last_name": "Lorens",
-    "username": "dsquierd",
-    "email": "diliffed@uol.com.br",
-    "phone": "729-469-5926"
-}, {
-    "id": 15,
-    "first_name": "Anne-corinne",
-    "last_name": "Lorens",
-    "username": "acreesee",
-    "email": "adalesioe@webs.com",
-    "phone": "278-866-6311"
-}, {
-    "id": 16,
-    "first_name": "Kary",
-    "last_name": "Lorens",
-    "username": "kstockwellf",
-    "email": "keasbyf@dot.gov",
-    "phone": "330-760-0175"
-}, {
-    "id": 17,
-    "first_name": "Michele",
-    "last_name": "Lorens",
-    "username": "mokieltg",
-    "email": "mmotteg@vimeo.com",
-    "phone": "411-290-8227"
-}, {
-    "id": 18,
-    "first_name": "Reinwald",
-    "last_name": "Lorens",
-    "username": "rlombardoh",
-    "email": "rcoulth@godaddy.com",
-    "phone": "405-923-8391"
-}, {
-    "id": 19,
-    "first_name": "Tabbi",
-    "last_name": "Lorens",
-    "username": "tnestori",
-    "email": "tgowari@scribd.com",
-    "phone": "145-655-5217"
-}, {
-    "id": 20,
-    "first_name": "Ceil",
-    "last_name": "Lorens",
-    "username": "cthumannj",
-    "email": "cchessunj@howstuffworks.com",
-    "phone": "586-193-0476"
-}, {
-    "id": 21,
-    "first_name": "Harlene",
-    "last_name": "Lorens",
-    "username": "hcampagnek",
-    "email": "hjeroschk@archive.org",
-    "phone": "243-758-3577"
-}, {
-    "id": 22,
-    "first_name": "Ernst",
-    "last_name": "Lorens",
-    "username": "estrondl",
-    "email": "epassfieldl@csmonitor.com",
-    "phone": "829-305-9328"
-}, {
-    "id": 23,
-    "first_name": "Darsie",
-    "last_name": "Lorens",
-    "username": "doldfieldm",
-    "email": "djuaram@biblegateway.com",
-    "phone": "635-110-2385"
-}, {
-    "id": 24,
-    "first_name": "Aridatha",
-    "last_name": "Lorens",
-    "username": "akennyn",
-    "email": "ahaddockn@marriott.com",
-    "phone": "517-440-0230"
-}, {
-    "id": 25,
-    "first_name": "Analise",
-    "last_name": "Lorens",
-    "username": "acrystalo",
-    "email": "afazackerleyo@shareasale.com",
-    "phone": "314-299-4898"
-}, {
-    "id": 26,
-    "first_name": "Nicolas",
-    "last_name": "Lorens",
-    "username": "norpwoodp",
-    "email": "nvirginp@blogger.com",
-    "phone": "606-353-1532"
-}, {
-    "id": 27,
-    "first_name": "Mose",
-    "last_name": "Lorens",
-    "username": "mklawiq",
-    "email": "moconnollyq@nymag.com",
-    "phone": "630-993-1118"
-}, {
-    "id": 28,
-    "first_name": "Daisi",
-    "last_name": "Lorens",
-    "username": "dtroppmannr",
-    "email": "dsergeantr@vkontakte.ru",
-    "phone": "381-138-5867"
-}, {
-    "id": 29,
-    "first_name": "Pat",
-    "last_name": "Lorens",
-    "username": "pshorys",
-    "email": "psuters@tmall.com",
-    "phone": "543-121-2704"
-}, {
-    "id": 30,
-    "first_name": "Coral",
-    "last_name": "Lorens",
-    "username": "cmcmickant",
-    "email": "cjolint@blinklist.com",
-    "phone": "668-403-7732"
-}
-    , {
-        "id": 31,
-        "first_name": "Coral",
-        "last_name": "Lorens",
-        "username": "cmcmickant",
-        "email": "cjolint@blinklist.com",
-        "phone": "668-403-7732"
-    }, {
-        "id": 32,
-        "first_name": "Coral",
-        "last_name": "Lorens",
-        "username": "cmcmickant",
-        "email": "cjolint@blinklist.com",
-        "phone": "668-403-7732"
-    }, {
-        "id": 33,
-        "first_name": "Coral",
-        "last_name": "Lorens",
-        "username": "cmcmickant",
-        "email": "cjolint@blinklist.com",
-        "phone": "668-403-7732"
-    }];
-
-
-
-
-
-
-
 $(document).ready(function() {
 
-    data = mock_data_2;
-    for (var i = 0; i < data.length; i++) {
-        var table_row = '<tr><td>'+data[i]['first_name']+'</td><td>'+data[i]['last_name']+'</td><td>'+data[i]['username']+'</td><td>'+data[i]['email']+'</td><td><button type="button" class="btn btn-danger">Ban</button></td></tr>';
-        $( ".parents-table tbody" ).append( table_row );
-    }
+    $.ajax({
+        url: "/app/admin/clients", // url where to submit the request
+        type : "GET", // type of action POST || GET
+        dataType : 'json', // data type
+        //contentType : 'json',
+        success : function(result) {
+            // you can see the result from the console
+            // tab of the developer tools
+            if(result.length != 0) {
+                data = result;
+                for (var i = 0; i < data.length; i++) {
+                    var table_row = '<tr><td>'+data[i]['first_name']+'</td><td>'+data[i]['last_name']+'</td><td>'+data[i]['username']+'</td><td>'+data[i]['email']+'</td><td>'+data[i]['m_phone']+'</td><td>'+data[i]['wallet']+'</td><td><button onclick="location.href ='+"'" +' /app/admin/clients/reset/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-success">Reset</button><button onclick="location.href ='+"'" +' /app/admin/clients/block/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-warning">Ban</button><button onclick="location.href ='+"'" +' /app/admin/clients/delete/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-primary">Remove</button></td></tr>';
+                    $( ".parents-table tbody" ).append( table_row );
+                }
+
+            }
+        },
+        error: function(xhr, resp, text) {
+            alert("AJAX FAILED");
+        }
+    })
+
+
 	
 
 
@@ -465,13 +53,6 @@ $(document).ready(function() {
 
 
 
-data = mock_data_1;
-for (var i = 0; i < data.length; i++) {
-    var table_row = '<tr><td>'+data[i]['name']+'</td><td>'+data[i]['username']+'</td><td>'+data[i]['phone']+'</td><td>'+data[i]['email']+'</td><td><button type="button" class="btn btn-default">button</button></td></tr>';
-    $( ".providers-table tbody" ).append( table_row );
-}
-
-	
 	
 	
 
@@ -514,6 +95,8 @@ $(function() {
             '              <th>Επώνυμο</th>\n' +
             '              <th>Username</th>\n' +
             '              <th>Email</th>\n' +
+            '              <th>Τηλέφωνο</th>\n' +
+            '              <th>Πορτοφόλι</th>\n' +
             '              <th>Ενέργειες</th>\n' +
             '            </tr>\n' +
             '          </thead>\n' +
@@ -522,11 +105,27 @@ $(function() {
             '          </tbody>\n' +
             '        </table>' ;
         $('#usprovTable').append(addUs);
-        data = mock_data_2;
-        for (var i = 0; i < data.length; i++) {
-            var table_row = '<tr><td>'+data[i]['first_name']+'</td><td>'+data[i]['last_name']+'</td><td>'+data[i]['username']+'</td><td>'+data[i]['email']+'</td><td><button type="button" class="btn btn-danger">Ban</button></td></tr>';
-            $( ".parents-table tbody" ).append( table_row );
-        }
+        $.ajax({
+            url: "/app/admin/clients", // url where to submit the request
+            type : "GET", // type of action POST || GET
+            dataType : 'json', // data type
+            //contentType : 'json',
+            success : function(result) {
+                // you can see the result from the console
+                // tab of the developer tools
+                if(result.length != 0) {
+                    data = result;
+                    for (var i = 0; i < data.length; i++) {
+                        var table_row = '<tr><td>'+data[i]['first_name']+'</td><td>'+data[i]['last_name']+'</td><td>'+data[i]['username']+'</td><td>'+data[i]['email']+'</td><td>'+data[i]['m_phone']+'</td><td>'+data[i]['wallet']+'</td><td><button onclick="location.href ='+"'" +' /app/admin/clients/reset/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-success">Reset</button><button onclick="location.href ='+"'" +' /app/admin/clients/block/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-warning">Ban</button><button onclick="location.href ='+"'" +' /app/admin/clients/delete/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-primary">Remove</button></td></tr>';
+                        $( ".parents-table tbody" ).append( table_row );
+                    }
+
+                }
+            },
+            error: function(xhr, resp, text) {
+                alert("AJAX FAILED");
+            }
+        })
         e.preventDefault();
     });
     $('#admins').click(function(e) {
@@ -540,9 +139,20 @@ $(function() {
             '          <thead>\n' +
             '            <tr>\n' +
             '              <th>Όνομα</th>\n' +
+	    '              <th>Πόλη</th>\n' +
+	    '              <th>Οδός</th>\n' +
+	    '              <th>Αριθμός Οδού</th>\n' +
+	    '              <th>Τ.Κ</th>\n' +
             '              <th>Username</th>\n' +
             '              <th>Τηλέφωνο</th>\n' +
             '              <th>Email</th>\n' +
+	    '              <th>A.Φ.Μ</th>\n' +
+            '              <th>IBAN</th>\n' +
+	    '              <th>Όνομα Υπεύθυνου</th>\n' +
+            '              <th>Επώνυμο Υπεύθυνου</th>\n' +
+	    '              <th>Tηλέφωνο Υπεύθυνου</th>\n' +
+	    '              <th>Συνδρομή</th>\n' +
+	    '              <th>Χρέος</th>\n' +
             '              <th>Ενέργειες</th>\n' +
             '            </tr>\n' +
             '          </thead>\n' +
@@ -551,11 +161,39 @@ $(function() {
             '          </tbody>\n' +
             '        </table>';
         $('#usprovTable').append(addAdm);
-        data = mock_data_1;
-        for (var i = 0; i < data.length; i++) {
-            var table_row = '<tr><td>'+data[i]['name']+'</td><td>'+data[i]['username']+'</td><td>'+data[i]['phone']+'</td><td>'+data[i]['email']+'</td><td><button type="button" class="btn btn-success">Reset</button><button type="button" class="btn btn-warning">Ban</button><button type="button" class="btn btn-zzz">Remove</button></td></tr>';
-            $( ".providers-table tbody" ).append( table_row );
-        }
+        $.ajax({
+            url: "/app/admin/providers", // url where to submit the request
+            type : "GET", // type of action POST || GET
+            dataType : 'json', // data type
+            //contentType : 'json',
+            success : function(result) {
+                // you can see the result from the console
+                // tab of the developer tools
+                if(result.length != 0) {
+                    data = result;
+                    for (var i = 0; i < data.length; i++) {
+			var date = new Date(data[i]['subscription']);
+                	var dd = date.getDate();
+                    	var mm = date.getMonth();
+                    	var yyyy = date.getFullYear();
+		    	if (dd<10){
+                       	 dd = '0' + dd;
+                    	}
+                   	if (mm<10){
+                         mm = '0' +mm;
+                     	}
+		    	var subDate = dd + "/" + mm + "/" + yyyy;
+                        var table_row = '<tr><td>'+data[i]['company_name']+'</td><td>'+data[i]['city']+'</td><td>'+data[i]['address']+'</td><td>'+data[i]['address_num']+'</td><td>'+data[i]['postal_code']+'</td><td>'+data[i]['username']+'</td><td>'+data[i]['phone']+'</td><td>'+data[i]['email']+'</td><td>'+data[i]['afm']+'</td><td>'+data[i]['iban']+'</td><td>'+data[i]['first_name']+'</td><td>'+data[i]['last_name']+'</td><td>'+data[i]['m_phone']+'</td><td>'+subDate+'</td><td>'+data[i]['debt']+'</td><td><button onclick="location.href ='+"'" +' /app/admin/providers/reset/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-success">Reset</button><button onclick="location.href ='+"'" +' /app/admin/providers/block/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-warning">Ban</button><button onclick="location.href ='+"'" +' /app/admin/providers/delete/'+data[i]["id"]+"'"+';"  type="button" class="btn btn-primary">Remove</button></td></tr>';
+                        $( ".providers-table tbody" ).append( table_row );
+                    }
+
+                }
+            },
+            error: function(xhr, resp, text) {
+                alert("AJAX FAILED");
+            }
+        })
+
         e.preventDefault();
     });
 });
